15209e679c88574ed1f929155d99f208c3c017ae
Kush Patel <kp2zf@virginia.edu>
2018-07-02 01:29:50 -0400

    added the delete button functionality

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -46,4 +46,11 @@ app.get('/api/posts', (req, res, next) => {
   });
 });
 
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ messgae: 'Post deleted!' });
+  });
+});
+
 module.exports = app;
--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -17,17 +17,21 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   private postsSub: Subscription;
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService) { }
 
   ngOnInit() {
     this.postsService.getPosts();
     this.postsSub = this.postsService.getPostUpdateListener()
-    .subscribe((posts: Post[]) => {
-      this.posts = posts;
-    });
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
   }
 
-    ngOnDestroy() {
-      this.postsSub.unsubscribe();
-    }
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
