a09124d7eedbac65821df4d2b5dac094e8b23fdf
Kush Patel <kp2zf@virginia.edu>
2018-07-02 01:54:35 -0400

    fixed post delete issue

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -31,9 +31,12 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(result => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost
+      ._id
+  });
   });
 });
 
@@ -49,7 +52,7 @@ app.get('/api/posts', (req, res, next) => {
 app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
-    res.status(200).json({ messgae: 'Post deleted!' });
+    res.status(200).json({ message: 'Post deleted!' });
   });
 });
 
