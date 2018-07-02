65bc1f86f6028ce2437ec5f7e352f7221b27bfea
Kush Patel <kp2zf@virginia.edu>
2018-07-02 01:06:51 -0400

    transformed response data

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
 
+mongoose.connect("mongodb+srv://kush:ZakvitRbg9tOiuPn@cluster0-zsux9.mongodb.net/node-angular?retryWrites=true")
+  .then(() => {
+    console.log('Connected to Database!');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,27 +27,22 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: 'fadf12421l',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    { id: 'dgshthnhtfg',
-      title: 'Second server-side post',
-      content: 'This is coming from the server!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched sucessfully!',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched sucessfully!',
+      posts: documents
+      });
   });
 });
 
