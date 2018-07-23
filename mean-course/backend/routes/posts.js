0131001814ec36e8ff32a8f5a069d8b448bef185
Kush Patel <kp2zf@virginia.edu>
2018-07-23 14:59:07 -0400

    error handling

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const multer = require("multer");
-//checkAuth
 const Post = require("../models/post");
 const checkAuth = require("../middleware/check-auth");
-
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
@@ -28,8 +26,10 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post("", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
-const url = req.protocol + '://' + req.get("host");
+router.post("", checkAuth, multer({
+  storage: storage
+}).single("image"), (req, res, next) => {
+  const url = req.protocol + '://' + req.get("host");
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
@@ -37,17 +37,24 @@ const url = req.protocol + '://' + req.get("host");
     creator: req.userData.userId
   });
   post.save().then(createdPost => {
-    res.status(201).json({
-      message: "Post added successfully",
-      post: {
-        ...createdPost,
-        id: createdPost._id,
-      }
+      res.status(201).json({
+        message: "Post added successfully",
+        post: {
+          ...createdPost,
+          id: createdPost._id,
+        }
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Creating a post failed!"
+      });
     });
-  });
 });
 
-router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
+router.put("/:id", checkAuth, multer({
+  storage: storage
+}).single("image"), (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
     const url = req.protocol + '://' + req.get("host");
@@ -60,12 +67,24 @@ router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req, 
     imagePath: imagePath,
     creator: req.userData.userId
   });
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+  Post.updateOne({
+    _id: req.params.id,
+    creator: req.userData.userId
+  }, post).then(result => {
     if (result.nModified > 0) {
-      res.status(200).json({ message: "Update successful!" });
+      res.status(200).json({
+        message: "Update successful!"
+      });
     } else {
-    res.status(401).json({ message: "Not Authorized!" });
-  }
+      res.status(401).json({
+        message: "Not Authorized!"
+      });
+    }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Couldn't update post!"
+    });
   });
 });
 
@@ -80,16 +99,21 @@ router.get("", (req, res, next) => {
       .limit(pageSize);
   }
   postQuery.then(documents => {
-    fetchedPosts = documents;
-    return Post.count();
-  })
-  .then(count => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: fetchedPosts,
-      maxPosts: count
+      fetchedPosts = documents;
+      return Post.count();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: fetchedPosts,
+        maxPosts: count
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fetching posts failed!"
+      });
     });
-  });
 });
 
 router.get("/:id", (req, res, next) => {
@@ -97,20 +121,38 @@ router.get("/:id", (req, res, next) => {
     if (post) {
       res.status(200).json(post);
     } else {
-      res.status(404).json({ message: "Post not found!" });
+      res.status(404).json({
+        message: "Post not found!"
+      });
     }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Fetching post failed!"
+    });
   });
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
+  Post.deleteOne({
+    _id: req.params.id,
+    creator: req.userData.userId
+  }).then(result => {
     if (result.n > 0) {
-      res.status(200).json({ message: "Deletion successful!" });
+      res.status(200).json({
+        message: "Deletion successful!"
+      });
     } else {
-    res.status(401).json({ message: "Not Authorized!" });
+      res.status(401).json({
+        message: "Not Authorized!"
+      });
     }
-  }
-);
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Fetching posts failed!"
+    });
+  });
 });
 
 module.exports = router;
